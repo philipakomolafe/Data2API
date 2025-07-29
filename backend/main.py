558e800eeec834ae8fcdef3a52217eba46d37f55
Philip Akomolfe <akomolafephilip@gmail.com>
2025-07-29 07:19:35 +0100

    Added Authentication

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from api import upload  # adjust this path to your actual file
+from api import upload, auth # adjust this path to your actual file
 
 app = FastAPI()
 app.include_router(upload.router, prefix="/upload", tags=["upload"])
+
+app.include_rounter(auth.router, prefix="/auth")
