653f283732d5ed209e6e8307e8e1783ea77e3557
Philip Akomolfe <akomolafephilip@gmail.com>
2025-07-30 01:31:16 +0100

    Added Authentication

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,6 +1,6 @@
 import os
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from supabase import create_client, Client
 from dotenv import load_dotenv
 from typing import Optional
