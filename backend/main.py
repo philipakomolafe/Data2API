c4ed9993be4596946f24a6de5b21590d7d471185
Philip Akomolfe <akomolafephilip@gmail.com>
2025-07-31 08:25:03 +0100

    Set-up Frontend and Backend

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import upload, auth
 from pipelines import infer_pipeline
 
 app = FastAPI()
 
-# Include the router from Upload.py
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# Include routers from API files
 app.include_router(upload.router, prefix="/upload", tags=["File Operations"])
-
-# Include new router instance from Auth.py
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
-
-# Include the pipelines router...
 app.include_router(infer_pipeline.router, prefix='/pipelines', tags=['Pipelines'])
